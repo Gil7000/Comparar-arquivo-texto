861bb5833cf61f34920f26354fcc3e9b74eb7ec4
Gilmar Silva <gil1965@gmail.com>
2021-11-17 20:29:52 -0300

    Versão 002
    Utilizando funções para diminuir a repetição de código

--- Comparar_txt.py
+++ Comparar_txt.py
@@ -5,58 +5,48 @@
 import os
 import time
 
+def file_exist(arq):
+    if not os.path.isfile(arq):
+        print("Arquivo "+arq+" não localizado")
+        print("Finalizando o programa.")
+        exit()
+
+def open_file(arq):
+    print('Abrindo arquivo ['+arq+'].......................: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+
+    filearq = open(arq, 'r', encoding='ANSI')  # abre o arquivo no modo de leitura
+    conteudo_arq = filearq.readlines()
+
+    # insere o conteúdo do arquivo oldfile.txt na lista oldfile
+    listarq = []
+    for i in range(len(conteudo_arq)):
+        listarq.append(conteudo_arq[i])
+    filearq.close()  # fecha o arquivo
+
+    print('Quantidade de registros no arquivo '+arq+' .....: ' + str(len(listarq)))
+    return listarq
+
 os.chdir('.\Files')  # altera para esse diretório - Windows
 # os.chdir('Files/')    # altera para esse diretório - Mac Os
 print('Diretório de trabalho...............................: ' + os.getcwd())
 print('Inicio..............................................: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
+
 # verificar se os arquivos existem no diretório
 
-if not os.path.isfile('oldfile.txt'):
-    print("Arquivo oldfile.txt não localizado")
-    print("Finalizando o programa.")
-    exit()
-elif not os.path.isfile('newfile.txt'):
-    print("Arquivo newfile.txt não localizado")
-    print("Finalizando o programa.")
-    exit()
-else:
-    while True:
-        cab = input("O arquivo possuí cabeçalho? (S)im (N)ão ")
-        if cab.upper() in ("S", "N"):
-            break
+file_exist('oldfile.txt')
+file_exist('newfile.txt')
 
-#inicio do tratamento do arquivo oldfile.txt
-print('Abrindo arquivo [oldfile.txt]........................: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+while True:
+    cab = input("O arquivo possuí cabeçalho? (S)im (N)ão ")
+    if cab.upper() in ("S", "N"):
+        break
 
-fileoldfile = open('oldfile.txt', 'r', encoding='ANSI')  # abre o arquivo oldfile.txt no modo de leitura
-conteudo_old = fileoldfile.readlines()
+#Salva o conteúdo dos arquivos numa LIST
+oldfile = open_file('oldfile.txt')
+newfile = open_file('newfile.txt')
 
-# insere o conteúdo do arquivo oldfile.txt na lista oldfile
-oldfile = []
-for i in range(len(conteudo_old)):
-    oldfile.append(conteudo_old[i])
-fileoldfile.close()  # fecha o arquivo
-
-print('Quantidade de registros no arquivo oldfile...........: ' + str(len(oldfile)))
-#Fim do tratamento do arquivo oldfile.txt
-
-#inicio do tratamento do arquivo newfile.txt
-print('Abrindo arquivo [newfile.txt]........................: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-
-filenewfile = open('newfile.txt', 'r', encoding='ANSI')  # abre o arquivo newfile.txt no modo de leitura
-conteudo_new = filenewfile.readlines()
-
-# insere o conteúdo do arquivo newfile.txt na lista newfile
-newfile = []
-for i in range(len(conteudo_new)):
-    newfile.append(conteudo_new[i])
-filenewfile.close()  # fecha o arquivo
-
-print('Quantidade de registros no arquivo newfile...........: ' + str(len(newfile)))
-#fim do tratamento do arquivo newfile.txt
-
-print('Separando as linhas diferentes entre os arquivos.....: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+print('Separando as linhas diferentes entre os arquivos....: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 # insere na lista result apenas as linhas que existem em newfile e não existem em oldfile
 result = sorted(list(set(newfile) - set(oldfile)))  #o arquivo resultado esta sendo ordenado, caso não queira, substitua o comando para [result = list(set(newfile) - set(oldfile))]
 cab == ""
